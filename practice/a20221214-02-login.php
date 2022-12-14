0c6d4a5e265911355fa24ff5c597c38699435d9f
Shinder Lin <shinder.lin@gmail.com>
2022-12-14 11:00:10 +0800

    登入帳密 1

--- practice/a20221214-02-login.php
+++ practice/a20221214-02-login.php
@@ -1,32 +1,69 @@
 <?php
-  session_start(); # 啟用 session 功能
+session_start(); # 啟用 session 功能
+
+$users = [
+  'shin' => [
+    'pw' => '2345',
+    'nickname' => '小新'
+  ],
+  'ming' => [
+    'pw' => '5678',
+    'nickname' => '小明'
+  ],
+];
+
+if (!empty($_POST)) {
+  # 如果用戶端有送表單資料出來
+
+  if (!empty($_POST['account']) and !empty($_POST['password'])) {
+    # 如果兩欄都有值
+
+    if (!empty($users[$_POST['account']])) {
+      # 帳號是對的
+
+      $user = $users[$_POST['account']];
+      if ($_POST['password'] === $user['pw']) {
+        # 密碼也是對的
+
+        $_SESSION['user'] = [
+          'account' => $_POST['account'],
+          'nickname' => $user['nickname'],
+        ];
+      }
+    }
+  }
+}
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
 
-<pre><?php 
-  if(isset($_POST['account'])){
-    print_r($_POST);
-  }
-?></pre>
-<form name="form1" method="post">
-  <input type="text" name="account">
-  <br>
-  <input type="password" name="password">
-  <br>
-  <input type="submit">
-</form>
+  <?php if (isset($_SESSION['user'])) : ?>
+    <h2><?= $_SESSION['user']['nickname'] ?> 您好</h2>
+  <?php else : ?>
+    <form name="form1" method="post">
+      <input type="text" name="account">
+      <br>
+      <input type="password" name="password">
+      <br>
+      <input type="submit">
+    </form>
+  <?php endif ?>
+
 
 
 
 
 </body>
+
 </html>
