9520152a10bdf8933114e3f12169ad2717d4d0e3
Shinder Lin <shinder.lin@gmail.com>
2022-12-15 10:46:13 +0800

    算總頁數

--- address-book/list.php
+++ address-book/list.php
@@ -8,20 +8,40 @@ if($page<1){
   exit;
 }
 
+$t_sql = "SELECT COUNT(1) FROM address_book";
+// 總筆數
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 
-$sql = sprintf(
-  "SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s",
-  ($page - 1) * $perPage,
-  $perPage
-);
+// 總頁數
+$totalPages = ceil($totalRows/$perPage);
 
-$rows = $pdo->query($sql)->fetchAll();
+$rows = [];
+// 如果有資料的話
+if(! empty($totalRows)){
+  if($page > $totalPages){
+    // 頁碼超出範圍時, 轉向到最後一頁
+    header('Location: ?page='. $totalPages);
+    exit;
+  }
+
+  $sql = sprintf(
+    "SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s",
+    ($page - 1) * $perPage,
+    $perPage
+  );
+  $rows = $pdo->query($sql)->fetchAll();
+}
+
+
+
+
 
 
 ?>
 <?php include './parts/html-head.php' ?>
 <?php include './parts/navbar.php' ?>
 <div class="container">
+  <div><?= $totalRows ?></div>
   <div class="row">
     <div class="col">
       <table class="table table-striped table-bordered">
