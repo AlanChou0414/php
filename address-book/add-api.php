52e88d51be108f7517ccd56f49636f52b872fd39
Shinder Lin <shinder.lin@gmail.com>
2022-12-15 14:11:26 +0800

    add-api 2

--- address-book/add-api.php
+++ address-book/add-api.php
@@ -17,6 +17,24 @@ if(empty($_POST['name'])) {
 
 // TODO: 欄位資料檢查
 
+$sql = "INSERT INTO `address_book`(
+  `name`, `email`, `mobile`, 
+  `birthday`, `address`, `created_at`
+  ) VALUES (
+    ?,?,?,
+    ?,?, NOW()
+  )";
 
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+  $_POST['name'],
+  $_POST['email'],
+  $_POST['mobile'],
+  $_POST['birthday'],
+  $_POST['address'],
+]);
+
+$output['success'] = !! $stmt->rowCount();
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
