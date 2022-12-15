e210308299b5b3e76a861c2a6a9e60556611971f
Shinder Lin <shinder.lin@gmail.com>
2022-12-15 15:45:41 +0800

    表單的後端驗證

--- address-book/add-api.php
+++ address-book/add-api.php
@@ -17,6 +17,19 @@ if(empty($_POST['name'])) {
 
 // TODO: 欄位資料檢查
 
+$isPass = true; // 是否通過檢查
+
+if(mb_strlen($_POST['name']) < 2){
+  $output['errors']['name'] = '請填寫正確的姓名';
+  $isPass = false;
+}
+
+if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)===false){
+  $output['errors']['email'] = '請填寫正確的 email';
+  $isPass = false;
+}
+
+
 $sql = "INSERT INTO `address_book`(
   `name`, `email`, `mobile`, 
   `birthday`, `address`, `created_at`
@@ -35,15 +48,17 @@ if(!empty($_POST['birthday'])){
     $birthday = date('Y-m-d', $t);
   }
 }
+if($isPass) {
+  $stmt->execute([
+    $_POST['name'],
+    $_POST['email'],
+    $_POST['mobile'] ?? '',
+    $birthday,
+    $_POST['address'] ?? '',
+  ]);
+  
+  $output['success'] = !! $stmt->rowCount();
+}
 
-$stmt->execute([
-  $_POST['name'],
-  $_POST['email'],
-  $_POST['mobile'],
-  $birthday,
-  $_POST['address'],
-]);
-
-$output['success'] = !! $stmt->rowCount();
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
