6ea4b04c6bde24ec64704e300167a51f931ad743
Shinder Lin <shinder.lin@gmail.com>
2022-12-15 11:18:33 +0800

    當前的頁碼active

--- address-book/list.php
+++ address-book/list.php
@@ -45,15 +45,19 @@ if (!empty($totalRows)) {
     <div class="col">
       <nav aria-label="Page navigation example">
         <ul class="pagination">
-          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item <?= $page==1 ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= $page-1 ?>">Previous</a>
+          </li>
 
           <?php for($i=1; $i<=$totalPages; $i++): ?>
-          <li class="page-item">
+          <li class="page-item <?= $i==$page ? 'active' : '' ?> ">
             <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
           </li>
           <?php endfor ?>
 
-          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+          <li class="page-item <?= $page==$totalPages ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= $page+1 ?>">Next</a>
+          </li>
         </ul>
       </nav>
     </div>
