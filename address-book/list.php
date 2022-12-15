77f1fe1eac6a127ce5f323557d5c974e83d64d00
Shinder Lin <shinder.lin@gmail.com>
2022-12-15 10:56:41 +0800

    分頁按鈕

--- address-book/list.php
+++ address-book/list.php
@@ -1,9 +1,9 @@
 <?php
 require './parts/connect_db.php';
 
-$perPage = 10;
+$perPage = 20;
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-if($page<1){
+if ($page < 1) {
   header('Location: ?page=1');
   exit;
 }
@@ -13,14 +13,14 @@ $t_sql = "SELECT COUNT(1) FROM address_book";
 $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 
 // 總頁數
-$totalPages = ceil($totalRows/$perPage);
+$totalPages = ceil($totalRows / $perPage);
 
 $rows = [];
 // 如果有資料的話
-if(! empty($totalRows)){
-  if($page > $totalPages){
+if (!empty($totalRows)) {
+  if ($page > $totalPages) {
     // 頁碼超出範圍時, 轉向到最後一頁
-    header('Location: ?page='. $totalPages);
+    header('Location: ?page=' . $totalPages);
     exit;
   }
 
@@ -41,7 +41,23 @@ if(! empty($totalRows)){
 <?php include './parts/html-head.php' ?>
 <?php include './parts/navbar.php' ?>
 <div class="container">
-  <div><?= $totalRows ?></div>
+  <div class="row">
+    <div class="col">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+
+          <?php for($i=1; $i<=$totalPages; $i++): ?>
+          <li class="page-item">
+            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+          </li>
+          <?php endfor ?>
+
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
   <div class="row">
     <div class="col">
       <table class="table table-striped table-bordered">
