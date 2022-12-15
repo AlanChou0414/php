b837b3d60200216041390e6be5cced6d334cd348
Shinder Lin <shinder.lin@gmail.com>
2022-12-15 09:41:13 +0800

    拆解頁面

---
+++ address-book/index_.php
@@ -0,0 +1,7 @@
+<?php include './parts/html-head.php' ?>
+<?php include './parts/navbar.php' ?>
+<div class="container">
+  <h2>index_</h2>
+</div>
+<?php include './parts/scripts.php' ?>
+<?php include './parts/html-foot.php' ?>
