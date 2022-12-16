7ba99db77470c3493fe9aeac5c87fd35bdaaf191
Shinder Lin <shinder.lin@gmail.com>
2022-12-16 09:57:35 +0800

    改善 add-api

--- address-book/add-api.php
+++ address-book/add-api.php
@@ -19,12 +19,20 @@ if(empty($_POST['name'])) {
 
 $isPass = true; // 是否通過檢查
 
-if(mb_strlen($_POST['name']) < 2){
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$mobile = $_POST['mobile'] ?? '';
+$birthday = $_POST['birthday'] ?? '';
+$address = $_POST['address'] ?? '';
+
+
+
+if(mb_strlen($name ) < 2){
   $output['errors']['name'] = '請填寫正確的姓名';
   $isPass = false;
 }
 
-if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)===false){
+if(filter_var($email, FILTER_VALIDATE_EMAIL)===false){
   $output['errors']['email'] = '請填寫正確的 email';
   $isPass = false;
 }
@@ -41,20 +49,22 @@ $sql = "INSERT INTO `address_book`(
 $stmt = $pdo->prepare($sql);
 
 
-$birthday = null;
 if(!empty($_POST['birthday'])){
   $t = strtotime($_POST['birthday']);
   if($t!==false){
     $birthday = date('Y-m-d', $t);
   }
 }
+if(empty($birthday)){
+  $birthday = null;
+}
 if($isPass) {
   $stmt->execute([
-    $_POST['name'],
-    $_POST['email'],
-    $_POST['mobile'] ?? '',
+    $name,
+    $email,
+    $mobile,
     $birthday,
-    $_POST['address'] ?? '',
+    $address,
   ]);
   
   $output['success'] = !! $stmt->rowCount();
--- readme.txt
+++ readme.txt
@@ -4,4 +4,12 @@ wins:
 mac:
     ifconfig
 
-# test
+CRUD
+    create, read, update, delete
+
+Restful api
+    GET: 讀取資料
+    POST: 新增資料
+    PUT: 修改資料
+    DELETE: 刪除資料
+    
