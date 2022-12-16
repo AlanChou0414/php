400dcc10ff3859b66b94921c99c4234d7dabdc25
Shinder Lin <shinder.lin@gmail.com>
2022-12-16 10:19:37 +0800

    修正 XSS 漏洞

--- address-book/list.php
+++ address-book/list.php
@@ -95,7 +95,10 @@ if (!empty($totalRows)) {
               <td><?= $r['email'] ?></td>
               <td><?= $r['mobile'] ?></td>
               <td><?= $r['birthday'] ?></td>
-              <td><?= $r['address'] ?></td>
+              <!--
+              <td><?= strip_tags($r['address']) ?></td>
+          -->
+              <td><?= htmlentities($r['address']) ?></td>
               <td>
                 <a href="edit.php?sid=<?= $r['sid'] ?>">
                   <i class="fa-solid fa-file-pen"></i>
