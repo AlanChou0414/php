b386a6d54fe5686ae27b7a40011baeb952391c34
Shinder Lin <shinder.lin@gmail.com>
2022-12-16 14:15:59 +0800

    登入的表單

---
+++ address-book/login.php
@@ -0,0 +1,44 @@
+<?php
+require './parts/connect_db.php';
+
+?>
+<?php include './parts/html-head.php' ?>
+<?php include './parts/navbar.php' ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">新增資料</h5>
+          <form name="form1" onsubmit="checkForm(event)">
+            <div class="mb-3">
+              <label for="account" class="form-label">帳號</label>
+              <input type="text" class="form-control" id="account" name="account" required>
+              <div class="form-text"></div>
+            </div>
+
+            <div class="mb-3">
+              <label for="password" class="form-label">密碼</label>
+              <input type="password" class="form-control" id="password" name="password">
+              <div class="form-text"></div>
+            </div>
+            <button type="submit" class="btn btn-primary">登入</button>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+<?php include './parts/scripts.php' ?>
+<script>
+  function checkForm(e){
+    e.preventDefault();
+    console.log(document.form1.account.value);
+    console.log(document.form1.password.value);
+  }
+
+</script>
+<?php include './parts/html-foot.php' ?>
