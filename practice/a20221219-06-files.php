ea7c28ed07d79f645d9993aee3d6181899f2638f
Shinder Lin <shinder.lin@gmail.com>
2022-12-19 10:27:08 +0800

    $_FILES 2

--- practice/a20221219-06-files.php
+++ practice/a20221219-06-files.php
@@ -16,4 +16,34 @@ echo json_encode($_FILES);
     }
 }
 
+
+// 多個檔案
+{
+    "photos": {
+        "name": [
+            "4.png",
+            "25.png"
+        ],
+        "full_path": [
+            "4.png",
+            "25.png"
+        ],
+        "type": [
+            "image/png",
+            "image/png"
+        ],
+        "tmp_name": [
+            "C:\\xampp\\tmp\\php9BAA.tmp",
+            "C:\\xampp\\tmp\\php9BAB.tmp"
+        ],
+        "error": [
+            0,
+            0
+        ],
+        "size": [
+            41099,
+            39472
+        ]
+    }
+}
 */
