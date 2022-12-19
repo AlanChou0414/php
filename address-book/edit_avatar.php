3569ddb010711918dc2f9a1d528425f6636a96e7
Shinder Lin <shinder.lin@gmail.com>
2022-12-19 16:14:36 +0800

    上傳avatar, 取消

--- address-book/edit_avatar.php
+++ address-book/edit_avatar.php
@@ -10,16 +10,50 @@ require './parts/connect_db.php';
   <div class="row">
     <div class="col-lg-4">
       <div class="card">
-        <img src="./../uploads/<?= empty($_SESSION['admin']['avatar']) ? '_default.png' : $_SESSION['admin']['avatar'] ?>" class="card-img-top" alt="...">
+        <input type="hidden" id="avatar_val" value="<?= $_SESSION['admin']['avatar'] ?>">
+
+        <img id="myimg" src="./../uploads/<?= empty($_SESSION['admin']['avatar']) ? '_default.png' : $_SESSION['admin']['avatar'] ?>" class="card-img-top" alt="...">
         <div class="card-body">
 
-          <button class="btn btn-primary">上傳</button>
+          <button class="btn btn-primary" onclick="f.click()">上傳</button>
           <button class="btn btn-danger">確定</button>
-          <button class="btn btn-warning">取消</button>
+          <button class="btn btn-warning" onclick="location.reload()">取消</button>
         </div>
+
+
+        <form name="form1" onsubmit="return false" style="display: none;">
+          <input type="file" name="avatar" accept="image/*" />
+        </form>
       </div>
     </div>
   </div>
 </div>
 <?php include './parts/scripts.php' ?>
+<script>
+      const f = document.form1.avatar;
+      const myimg = document.querySelector("#myimg");
+      const avatar_val = document.querySelector("#avatar_val");
+
+
+      f.onchange = async ()=>{
+        const fd = new FormData(document.form1);
+        const r = await fetch('./../practice/upload-single-img.php', {
+          method: 'POST',
+          body: fd
+        });
+        const data = await r.json();
+        console.log({data});
+        if(data.success){
+          // 成功上傳
+          myimg.src = './../uploads/' + data.filename;
+          avatar_val.value = data.filename
+
+        } else {
+          alert(data.error || '沒有上傳的檔案');
+        }
+      };
+
+
+
+</script>
 <?php include './parts/html-foot.php' ?>
